3453c1cf33d98707351c35b6657606d03ca5997d
Eren Esmer <erenesmerz@gmail.com>
2020-06-13 19:16:01 +0300

    Change todo ui

--- src/cljs/challenge/app/views.cljs
+++ src/cljs/challenge/app/views.cljs
@@ -1,43 +1,55 @@
 (ns challenge.app.views
   (:require
     [re-frame.core :as re-frame]
-    [challenge.app.subs :as subs]))
-
-
-;; main
-
-
-;; todo-checkbox
-(defn todo-checkbox [done]
-  [:div.todo-checkbox
-   {:class (str (if done "done"))}])
+    [challenge.app.subs :as subs]
+    [challenge.icons :refer [archieve-icon delete-icon done-icon trash-icon]]))
 
 ;; todo-item
 (defn todo-item [todo]
-  [:div
-   {:class "todo-item"}
-   [:div
-    {:class "todo-data"}
-    [todo-checkbox (get todo :done)]
-    [:p.todo-title
+  [:div.todo-item
+   [:div.todo-data
+    [:h2.todo-title
      {:class (str (if (get todo :done) "done"))}
-     (get todo :title)]]
-   [:div
-    {:class "todo-toolbox"}
-    [:a {:href "#" :class "toggle-todo-button"} "⌘"]
-    [:a {:href "#" :class "delete-todo-button"} "⌦"]]
-   [:div {:class (str "todo-color " (get todo :color))}]])
+     (get todo :title)]
+    [:p.created-at (str "Created: " (get todo :created-at))]]
+   [:div.todo-toolbox
+    [:a.toggle-todo-button {:href "#"}
+     [done-icon]]
+    [:a.delete-todo-button {:href "#"}
+     [trash-icon]]]
+   [:div.todo-status-color {:class (if (get todo :done) "done")}]])
+
+;; add-todo
+(defn create-todo []
+  [:div.create-todo
+   [:input.create-todo-input {:type "text" :placeholder "Create To Do"}]
+   [:a.create-todo-button {:href "#"} [done-icon]]])
 
 ;; todo-container
 (defn todo-container [todos]
-  [todo-item
-   {:id 1 :title "this is an example todo item" :done true :color "blue"}])
+  [:<>
+   [create-todo]
+   (for [todo todos]
+     ^{:key (str (get todo :id))}
+     [todo-item
+      todo])])
 
 ;; app
-(defn app []
+(defn
+  app []
   (let [name (re-frame/subscribe [::subs/name])]
-    [:div {:class "todo-container"}
-     [todo-container]]))
+    [:div.todo-container
+     [todo-container
+      [{:id         1
+        :title      "this is an example todo item lorem sdjhfakdsjfhajksdhfjasd fasdfjkahsdfkjahdskf asdhjfajksdfhak dskfjhads hjkfa dksjfhaksjd fhakjds fhakjs dkfjhas dfha skjdfhj kasdajks dfhads"
+        :done       true
+        :created-at (str (js/Date.))
+        :color      "blue"}
+       {:id         2
+        :title      "this is an second example todo item"
+        :done       false
+        :created-at (str (js/Date.))
+        :color      "red"}]]]))
 
 ;(defn- panels [panel-name]
 ;  (case panel-name
