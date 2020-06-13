0b52b7fa16da338c52c2b8fb0a436a074ad0bcf3
Eren Esmer <erenesmerz@gmail.com>
2020-06-13 15:11:05 +0300

    Create some UI for todo in app.views

--- src/cljs/challenge/app/views.cljs
+++ src/cljs/challenge/app/views.cljs
@@ -1,30 +1,52 @@
 (ns challenge.app.views
   (:require
-   [re-frame.core :as re-frame]
-   [challenge.app.subs :as subs]
-   ))
+    [re-frame.core :as re-frame]
+    [challenge.app.subs :as subs]))
 
-
-(defn app []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1 (str "Hello from " @name ". This is the Home Page.")]
-
-     [:div
-      [:a {:href "#/about"}
-       "go to About Page"]]
-     ]))
 
 ;; main
 
-(defn- panels [panel-name]
-  (case panel-name
-    :app [app]
-    [:div]))
 
-(defn show-panel [panel-name]
-  [panels panel-name])
+;; todo-checkbox
+(defn todo-checkbox [done]
+  [:div.todo-checkbox
+   {:class (str (if done "done"))}])
 
-(defn main-panel []
-  (let [active-panel (re-frame/subscribe [::subs/active-panel])]
-    [show-panel @active-panel]))
+;; todo-item
+(defn todo-item [todo]
+  [:div
+   {:class "todo-item"}
+   [:div
+    {:class "todo-data"}
+    [todo-checkbox (get todo :done)]
+    [:p.todo-title
+     {:class (str (if (get todo :done) "done"))}
+     (get todo :title)]]
+   [:div
+    {:class "todo-toolbox"}
+    [:a {:href "#" :class "toggle-todo-button"} "⌘"]
+    [:a {:href "#" :class "delete-todo-button"} "⌦"]]
+   [:div {:class (str "todo-color " (get todo :color))}]])
+
+;; todo-container
+(defn todo-container [todos]
+  [todo-item
+   {:id 1 :title "this is an example todo item" :done true :color "blue"}])
+
+;; app
+(defn app []
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div {:class "todo-container"}
+     [todo-container]]))
+
+;(defn- panels [panel-name]
+;  (case panel-name
+;        :app [app]
+;        [:div]))
+;
+;(defn show-panel [panel-name]
+;  [panels panel-name])
+;
+;(defn main-panel []
+;  (let [active-panel (re-frame/subscribe [::subs/active-panel])]
+;    [show-panel @active-panel]))
