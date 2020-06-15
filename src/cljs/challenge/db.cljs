70eafe76c1951fae4ca71cd681e5a2ca7e74410a
Eren Esmer <erenesmerz@gmail.com>
2020-06-15 13:46:17 +0300

    Create specs for todos and app-db

--- src/cljs/challenge/db.cljs
+++ src/cljs/challenge/db.cljs
@@ -1,4 +1,27 @@
-(ns challenge.db)
+(ns challenge.db
+  (:require [cljs.spec.alpha :as s]))
 
+;; -- Specs
+(s/def ::int int?)
+(s/def ::title string?)
+(s/def ::done boolean?)
+(s/def ::created string?)
+(s/def ::todo (s/keys :req-un [::id ::title ::done ::created]))
+(s/def ::todos
+  (s/and (s/map-of ::id ::todo)
+         #(instance? PersistentTreeMap %)))
+
+(s/def ::showing
+  #{:all
+    :active
+    :done})
+
+(s/def ::name string?)
+
+(s/def ::db (s/keys :req-un [::todos ::showing ::name]))
+
+;; -- DB
 (def default-db
-  {:name "re-frame"})
+  {:name    "we love challengeZ"
+   :todos   (sorted-map)
+   :showing :all})
