9788616bceef70bf160b8e4ef906303e713546f1
Eren Esmer <erenesmerz@gmail.com>
2020-06-15 13:45:26 +0300

    Format file

---
+++ src/cljs/challenge/api.cljs
@@ -0,0 +1,30 @@
+(ns challenge.api
+  (:require
+    [day8.re-frame.http-fx]
+    [ajax.core :as ajax]))
+
+(defonce api-url "http://localhost:8000")
+
+(defn make-request [type url on-success on-failure]
+  {:http-xhrio
+   {
+    :method          type
+    :uri             (str api-url url)
+    :format          (ajax/json-request-format)
+    :response-format (ajax/json-response-format {:keywords? true})
+    :on-success      (if (vector? on-success) on-success [on-success])
+    :on-failure      (if (vector? on-failure) :common-on-failure [:common-on-failure])}}
+  )
+
+;(defn make-request
+;      [method url on-success on-failure]
+;      {:http-xhrio {:method          method
+;                    :uri             (str api-url url)
+;                    :format          (json-request-format)
+;                    :response-format (json-response-format)
+;                    {:keywords? true}
+;                                     :on-success
+;                    (if (vector? on-success) on-success [on-success])
+;                                     :on-failure
+;                    (if (vector? on-failure) on-failure [on-failure])
+;                    }})
